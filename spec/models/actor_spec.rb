c2a220ae794662a6ebd72deb66f6d64894ea0e35
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-19 10:27:54 -0600

    Build test and method for order by age young to old

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Actor do
     it { should have_many :movie_actors }
     it { should have_many(:movies).through(:movie_actors) }
   end
+
+  describe 'class methods' do
+    context '#order_by_age' do
+      it 'orders actors by age youngest to oldest' do
+        actor_1 = Actor.create!(name: 'Harrison Ford', age: 78)
+        actor_2 = Actor.create!(name: 'Brad Pitt', age: 57)
+        actor_3 = Actor.create!(name: 'Tom Hanks', age: 65)
+
+        expect(Actor.order_by_age).to eq([actor_2, actor_3, actor_1])
+      end
+    end
+  end
 end
