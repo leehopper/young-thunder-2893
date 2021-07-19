399d15c4bf1a0792d83180f37c44166ae0b0d700
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-19 10:23:11 -0600

    Build test and display for actor names

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe 'the movies show page' do
   before(:each) do
     @studio = Studio.create!(name: 'Mountain Films', location: 'Colorado')
     @movie = @studio.movies.create!(title: 'Hiking', creation_year: 2021, genre: 'documentary')
+    @actor_1 = @movie.actors.create!(name: 'Harrison Ford', age: 78)
+    @actor_2 = @movie.actors.create!(name: 'Brad Pitt', age: 57)
+    @actor_3 = @movie.actors.create!(name: 'Tom Hanks', age: 65)
   end
 
   it 'displays movie attributes' do
@@ -13,4 +16,14 @@ RSpec.describe 'the movies show page' do
     expect(page).to have_content(@movie.creation_year)
     expect(page).to have_content(@movie.genre)
   end
+
+  it 'displays movies actors' do
+    visit "/movies/#{@movie.id}"
+
+    expect(page).to have_content(@actor_1.name)
+    expect(page).to have_content(@actor_2.name)
+    expect(page).to have_content(@actor_3.name)
+  end
+
+  it 'orders actors by age oldest to youngest'
 end
