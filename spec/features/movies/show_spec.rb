221c03fd8f0985be62c8bc41829b3ea7a7da26b0
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-19 09:49:16 -0600

    Build route for movies show page

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe 'the movies show page' do
   before(:each) do
     @studio = Studio.create(name: 'Mountain Films', location: 'Colorado')
-    @movie = @studio_1.movies.create(title: 'Hiking', creation_year: 2021, genre: 'documentary')
+    @movie = @studio.movies.create(title: 'Hiking', creation_year: 2021, genre: 'documentary')
+  end
+
+  it 'displays movie attributes' do
+    visit "/movies/#{@movie.id}"
   end
 end
