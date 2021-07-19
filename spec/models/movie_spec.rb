429d79d6a2f26f42ff17c5c61c35a3ba3749385f
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-19 11:48:52 -0600

    Build test and method for add actor

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -6,4 +6,20 @@ RSpec.describe Movie do
     it { should have_many :movie_actors }
     it { should have_many(:actors).through(:movie_actors) }
   end
+
+  before (:each) do
+    @actor_1 = Actor.create!(name: 'Harrison Ford', age: 78)
+    @studio = Studio.create!(name: 'Mountain Films', location: 'Colorado')
+    @movie = @studio.movies.create!(title: 'Hiking', creation_year: 2021, genre: 'documentary')
+  end
+
+  describe 'instance methods' do
+    context '#add_actor' do
+      it 'adds and actor to the movie' do
+        @movie.add_actor(@actor_1.name)
+
+        expect(@movie.actors).to eq([@actor_1])
+      end
+    end
+  end
 end
