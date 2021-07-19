7fca3725a512c1c853170b59e17888b5cba959ef
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-19 09:42:25 -0600

    Build test for display movies below each studio

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -1,16 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'the studios index' do
+  before(:each) do
+    @studio_1 = Studio.create(name: 'Mountain Films', location: 'Colorado')
+    @studio_2 = Studio.create(name: 'Lake Films', location: 'Michigan')
+  end
 
   it 'displays studio name and location' do
-    studio_1 = Studio.create(name: 'Mountain Films', location: 'Colorado')
-    studio_2 = Studio.create(name: 'Lake Films', location: 'Michigan')
+    visit '/studios'
+
+    expect(page).to have_content(@studio_1.name)
+    expect(page).to have_content(@studio_1.name)
+    expect(page).to have_content(@studio_1.location)
+    expect(page).to have_content(@studio_1.location)
+  end
+
+  it 'displays moves for each studio' do
+    movie_1 = @studio_1.movies.create(title: 'Hiking', creation_year: 2021, genre: 'documentary')
+    movie_2 = @studio_1.movies.create(title: 'Rafting', creation_year: 2020, genre: 'action')
+    movie_3 = @studio_2.movies.create(title: 'Surfing', creation_year: 2019, genre: 'documentary')
 
     visit '/studios'
 
-    expect(page).to have_content(studio_1.name)
-    expect(page).to have_content(studio_1.name)
-    expect(page).to have_content(studio_1.location)
-    expect(page).to have_content(studio_1.location)
+    within("#studio-#{@studio_1.id}") do
+      expect(page).to have_content(movie_1.title)
+      expect(page).to have_content(movie_2.title)
+      expect(page).to_not have_content(movie_3.title)
+    end
+
+    within("#studio-#{@studio_2.id}") do
+      expect(page).to have_content(movie_3.title)
+      expect(page).to_not have_content(movie_1.title)
+      expect(page).to_not have_content(movie_2.title)
+    end
   end
 end
